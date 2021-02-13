20b4b049417b3f0dcde5d0ca68d1149ebb5f6eec
dani01S <dany_mccalister@yahoo.com>
2021-02-13 19:47:01 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const hamburgerMenu = document.querySelector(".small-menu-icon-container");
+
+hamburgerMenu.addEventListener("click",function(){
+  console.log("a");
+  hamburgerMenu.classList.toggle("change");
+});
